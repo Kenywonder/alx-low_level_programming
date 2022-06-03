2627e209b2ab26c2058ef2cf6b5b02fda261a2a9
your name <you@example.com>
2022-06-03 15:05:51 -0700

    new one

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,14 @@
-Try this out, puts("\"Programming is like building a multilingual puzzle");
+#include <stdio.h>
+
+/**
+ *
+ *  * main - main block
+ *
+ *   * Return: 0
+ *
+ *    */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
