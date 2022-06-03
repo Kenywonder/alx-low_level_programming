afac49d937436bb96a84925302180a165f573c29
your name <you@example.com>
2022-06-03 13:32:21 -0700

    my new

--- 0x00-hello_world/5-print.c
+++ 0x00-hello_world/5-print.c
@@ -1,1 +1,13 @@
-Print(word given)
+nclude <stdio.h>
+/**
+ *
+ *  * main - main block
+ *
+ *   * Return: 0
+ *
+ *    */
+int main(void)
+{
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+			return (0);
+}
