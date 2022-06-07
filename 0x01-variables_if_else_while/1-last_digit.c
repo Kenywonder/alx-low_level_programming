67e73de80cd4a90dd01c5defb7be5f64c53b24fe
your name <you@example.com>
2022-06-06 19:39:33 -0700

    my git

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,9 +4,9 @@
 
 /**
  * main - Prints the last digit of a randomly generated number
- *   and whether it is greater than 5, less than 6, or 0.
- *  
- *   Return: Always 0.
+ * and whether it is greater than 5, less than 6, or 0.
+ *
+ * Return: Always 0.
  */
 
 int main(void)
@@ -20,7 +20,7 @@ int main(void)
 	if ((n % 10) > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n",
-			n, n % 10);	
+			n, n % 10);
 	}
 	else if ((n % 10) < 6 && (n % 10) != 0)
 	{
@@ -32,5 +32,6 @@ int main(void)
 		printf("Last digit of %d is %d and is 0\n",
 			n, n % 10);
 	}
+
 	return (0);
 }
