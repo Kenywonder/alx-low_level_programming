3b9ae516f206c3545fdbbef320a16f0b99bcca95
your name <you@example.com>
2022-06-06 19:23:45 -0700

    my git

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- *  main - Prints the alphabet in lowercase, and then in uppercase.
- * 
- *  Return: Always 0.
+ * main - Prints the alphabet in lowercase, and then in uppercase.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
@@ -11,7 +11,7 @@ int main(void)
 
 	for (letter = 'a'; letter <= 'z'; letter++)
 		putchar(letter);
-	
+
 	for (letter = 'A'; letter <= 'Z'; letter++)
 		putchar(letter);
 
