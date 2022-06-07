92139462689b1ce2f521c0851184ef6717391e3d
your name <you@example.com>
2022-06-06 17:31:23 -0700

    my new

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
-/*
- * main - Prints the lowercase alphabet in reverse.
- *
- * Return: Always 0.
+/**
+ *  main - Entry point
+ *  Prints the lowercase alphabet in reverse.
+ * 
+ *  Return: Always 0
  */
 int main(void)
 {
@@ -11,7 +12,6 @@ int main(void)
 
 	for (letter = 'z'; letter >= 'a'; letter--)
 		putchar(letter);
-
 	putchar('\n');
 
 	return (0);
